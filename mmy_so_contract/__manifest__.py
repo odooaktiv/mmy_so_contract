61b2ace0f4d3fa47e8aa511b07fb7d4fc8887122
Parth Radadia <parth.radadia@aktivsoftware.com>
2023-09-28 12:02:42 +0530

    [ADD] Initial push

---
+++ mmy_so_contract/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "MMY Auto | SO Contract",
+    "summary": """
+        Manage multiple contracts on SO
+    """,
+    "description": """
+        Manage multiple contracts and approvals on SO
+    """,
+    "author": "MMY Auto",
+    "website": "",
+    "category": "Hidden",
+    "version": "16.0.0.0.0",
+    "license": "LGPL-3",
+    "depends": ["sale"],
+    "data": [
+        # "views/sale_order_views.xml",
+    ],
+    "auto_install": False,
+    "installable": True,
+    "application": False,
+}
